592cd9a8fc23c3dab61423a49ecb262a95f3c21e
davidphilz <143757096+davidphilz@users.noreply.github.com>
2025-04-28 06:27:30 +0100

    Add files via upload

---
+++ Payment Module/frontend/Styles/PaymentFailed.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const PaymentFailed = () => {
+    return (
+        <div>
+            <h1>Payment Rejected</h1>
+            <p> Your Payment was rejected. Please contact support or try again</p>
+        </div>
+    )
+}
+
+export default PaymentFailed
