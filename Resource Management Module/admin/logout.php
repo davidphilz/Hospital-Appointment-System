0295e8ddf51f8399a7bb56454df68a37794cd030
Gabriella <nelsongabriella700@gmail.com>
2025-04-30 12:08:19 +0100

    Update file Resource

--- Resource Management Module/admin/logout.php
+++ Resource Management Module/admin/logout.php
@@ -1,9 +1,12 @@
 <?php
 session_start();
+session_destroy();
 
-if (isset($_SESSION['admin'])){
-    unset($_SESSION['admin']);
+//if (isset($_SESSION['admin'])){
+  //  unset($_SESSION['admin']);
 
+   // header("Location:../index.php");
+//} else {
     header("Location:../index.php");
-}
+    exit();
 ?>
